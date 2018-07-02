f92c56eec2a07723fc1595a5e57af33946293af4
Marie-Christin Kloss <marie-christin.kloss@robotron.de>
2018-07-02 14:30:24 +0200

    tr

--- src/main/splunk/iqp/appserver/static/Startseite/js/start.js
+++
@@ -1,8 +0,0 @@
-var deps = [
-    "jquery",
-    "splunkjs/ready!",
-    "splunkjs/mvc/simplexml/ready!"
-];
-require(deps, function($) {
-    $("#panel1 h2.panel-title").prepend('<div style="text-align: center"><h2 style="color: #5379af;">FG-9 Innovation Lab &amp; TV-2-I</h2><img width="400px" src="/static/app/iqp/logo_without_background.png"></div>')
-});
